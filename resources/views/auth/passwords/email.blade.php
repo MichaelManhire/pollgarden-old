1e0545429e2c3d4529b75ebd4dd30cd439807a1c
Michael Manhire <michaelmanhire@gmail.com>
2020-05-04 01:59:06 -0700

    Update styling and layout of the password reset page

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,47 +1,43 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    <form method="POST" action="{{ route('password.email') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
+<div class="max-w-md mx-auto">
+    @if (session('status'))
+        <div class="alert alert-success" role="alert">
+            {{ session('status') }}
         </div>
+    @endif
+
+    <div class="text-center">
+        <img class="mx-auto" src="{{ asset('images/logo.svg') }}" alt="{{ config('app.name', 'Poll Garden') }}" height="64" width="64">
+        <h1 class="mt-4 text-3xl leading-9 font-extrabold">{{ __('Reset Password') }}</h1>
     </div>
+    <form class="px-4 py-8 mt-8 bg-white shadow sm:px-10 sm:rounded-lg" action="{{ route('login') }}" method="POST">
+        @csrf
+
+        <div>
+            <label class="text-sm font-medium leading-5 text-gray-700" for="email">{{ __('Email') }}</label>
+            <div class="mt-1 rounded-md shadow-sm">
+                <input class="form-input block w-full @error('email') border-red-300 text-red-900 @enderror"
+                       id="email"
+                       name="email"
+                       type="text"
+                       value="{{ old('email') }}"
+                       autocomplete="email"
+                       required
+                       @error('email')
+                       aria-invalid="true"
+                       aria-describedby="email-error"
+                       @enderror>
+            </div>
+            @error('email')
+                <p class="mt-2 text-sm text-red-600" id="email-error">{{ $message }}</p>
+            @enderror
+        </div>
+
+        <div class="mt-6 rounded-md shadow-sm">
+            <button class="flex justify-center w-full py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-green-600 hover:bg-green-500" type="submit">{{ __('Send Password Reset Link') }}</button>
+        </div>
+    </form>
 </div>
 @endsection
