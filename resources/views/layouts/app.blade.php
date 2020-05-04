c7a081e3d38394582f0d00e4f87d024ccc07f4a5
Michael Manhire <michaelmanhire@gmail.com>
2020-05-04 00:09:15 -0700

    Update default styling and layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html class="h-full text-gray-900" lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html class="h-full text-gray-900 bg-gray-100" lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -19,79 +19,98 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body class="flex flex-col h-full">
-    <header class="text-green-100 bg-green-800">
-        <div class="container flex items-center justify-between h-16">
-            <div class="flex items-center">
-                <a class="flex justify-start items-center flex-shrink-0 text-xl leading-none text-white" href="{{ url('/') }}">
-                    <img class="block h-8 w-auto" src="{{ asset('images/logo.svg') }}" alt="{{ config('app.name', 'Poll Garden') }}" height="32" width="32">
-                    <span class="hidden sm:block sm:ml-1">{{ config('app.name', 'Poll Garden') }}</span>
-                </a>
+    <header class="sticky top-0 bg-white shadow-sm">
+        <div class="container flex justify-between h-16">
+            <div class="flex">
+                @if (Request::is('/'))
+                    <h1 class="flex-shrink-0 flex items-center text-xl font-medium leading-none">
+                        <img class="block h-8 w-auto" src="{{ asset('images/logo.svg') }}" alt="{{ config('app.name', 'Poll Garden') }}" height="32" width="32">
+                        <span class="hidden sm:block sm:ml-1">{{ config('app.name', 'Poll Garden') }}</span>
+                    </h1>
+                @else
+                    <a class="flex-shrink-0 flex items-center text-xl font-medium leading-none" href="{{ url('/') }}">
+                        <img class="block h-8 w-auto" src="{{ asset('images/logo.svg') }}" alt="{{ config('app.name', 'Poll Garden') }}" height="32" width="32">
+                        <span class="hidden sm:block sm:ml-1">{{ config('app.name', 'Poll Garden') }}</span>
+                    </a>
+                @endif
 
-                <div class="ml-2 sm:ml-6">
-                    <nav class="flex">
-                        <a class="px-3 py-2 rounded-md text-sm leading-5 hover:text-white hover:bg-green-700" href="{{ route('polls.create') }}">
-                            {{ __('Create Poll') }}
-                        </a>
-                        <a class="sm:ml-4 px-3 py-2 rounded-md text-sm leading-5 hover:text-white hover:bg-green-700" href="{{ route('polls.index') }}">
-                            {{ __('View Polls') }}
-                        </a>
-                    </nav>
-                </div>
+                <nav class="flex ml-6" aria-label="Primary navigation">
+                    <ul class="flex">
+                        <li class="flex">
+                            @if (url()->current() === route('polls.create'))
+                                <span class="flex items-center px-1 pt-1 border-b-2 border-green-500 text-sm font-medium leading-5 text-gray-900" aria-current="true">
+                                    {{ __('Create Poll') }}
+                                </span>
+                            @else
+                                <a class="flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:border-gray-300 hover:text-gray-700" href="{{ route('polls.create') }}">
+                                    {{ __('Create Poll') }}
+                                </a>
+                            @endif
+                        </li>
+                        <li class="flex">
+                            @if (url()->current() === route('polls.index'))
+                                <span class="flex items-center px-1 pt-1 ml-8 border-b-2 border-green-500 text-sm font-medium leading-5 text-gray-900" aria-current="true">
+                                    {{ __('View Polls') }}
+                                </span>
+                            @else
+                                <a class="flex items-center px-1 pt-1 ml-8 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:border-gray-300 hover:text-gray-700" href="{{ route('polls.index') }}">
+                                    {{ __('View Polls') }}
+                                </a>
+                            @endif
+                        </li>
+                    </ul>
+                </nav>
             </div>
-            <div class="ml-2 sm:ml-6">
-                <div class="flex items-center">
-                    @guest
-                        <div class="flex-shrink-0">
-                            <a class="inline-flex items-center px-3 py-2 border border-gray-300 text-sm leading-4 font-medium rounded-md text-gray-700 bg-white hover:text-gray-500" href="{{ route('login') }}">
-                                {{ __('Log In') }}
-                            </a>
-                        </div>
+            <div class="flex items-center ml-6">
+                @guest
+                    <div class="flex-shrink-0">
+                        <a class="flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-green-700 bg-green-100 hover:bg-green-50" href="{{ route('login') }}">
+                            {{ __('Log In') }}
+                        </a>
+                    </div>
 
-                        <div class="ml-4 flex-shrink-0">
-                            <a class="inline-flex items-center px-3 py-2 border border-gray-300 text-sm leading-4 font-medium rounded-md text-gray-700 bg-white hover:text-gray-500" href="{{ route('register') }}">
-                                {{ __('Sign Up') }}
-                            </a>
-                        </div>
+                    <div class="ml-4 flex-shrink-0">
+                        <a class="flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-white bg-green-600 hover:bg-green-500" href="{{ route('register') }}">
+                            {{ __('Sign Up') }}
+                        </a>
+                    </div>
 
-                    @else
-                        <a class="p-1 border-2 border-transparent text-gray-400 rounded-full hover:text-white" href="#">
+                @else
+                    <div class="flex-shrink-0">
+                        <a class="text-gray-400 hover:text-gray-500" href="#" title="{{ __('Notifications') }}">
                             <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24" height="24" width="24" aria-hidden="true">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
                             </svg>
                             <span class="sr-only">{{ __('Notifications') }}</span>
                         </a>
+                    </div>
 
-                        <div class="ml-3">
-                            <div>
-                                <a class="block border-2 border-transparent rounded-full hover:border-gray-200" href="#">
-                                    <img class="h-8 w-8 rounded-full" src="https://source.unsplash.com/64x64/" alt="" height="32" width="32">
-                                    <span class="sr-only">{{ __('Profile') }}</span>
-                                </a>
-                            </div>
-                        </div>
-                    @endguest
-                </div>
+                    <div class="flex-shrink-0 ml-3 text-white">
+                        <a class="block rounded-full shadow-solid" href="#" title="{{ __('My Profile') }}">
+                            <img class="h-8 w-8 rounded-full" src="https://source.unsplash.com/64x64/" alt="" height="32" width="32">
+                            <span class="sr-only">{{ __('Profile') }}</span>
+                        </a>
+                    </div>
+                @endguest
             </div>
         </div>
     </header>
 
-    @yield('content')
+    <main class="flex-grow flex-shrink-0 container py-12">
+        @yield('content')
+    </main>
 
-    <footer class="flex-shrink-0 py-4 text-sm text-gray-500 bg-white border-t border-gray-100">
-        <div class="container md:flex md:justify-between md:items-center">
-            <ul class="flex justify-center mb-2 md:justify-around md:mb-0">
+    <footer class="flex-shrink-0 py-4 text-sm text-white bg-gray-900">
+        <div class="container flex justify-between items-center">
+            <ul class="flex justify-around">
                 <li>
-                    <a class="hover:text-gray-700 hover:underline" href="{{ url('/terms-of-use') }}">
-                        {{ __('Terms of Use') }}
-                    </a>
+                    <a class="hover:underline" href="{{ url('/terms-of-use') }}">{{ __('Terms of Use') }}</a>
                 </li>
                 <li class="ml-8">
-                    <a class="hover:text-gray-700 hover:underline" href="{{ url('/privacy-policy') }}">
-                        {{ __('Privacy Policy') }}
-                    </a>
+                    <a class="hover:underline" href="{{ url('/privacy-policy') }}">{{ __('Privacy Policy') }}</a>
                 </li>
             </ul>
-            <small class="block text-center text-sm md:text-left">&copy; {{ __('Copyright') }} {{ now()->year }} {{ config('app.name', 'Poll Garden') }}</small>
+            <small class="text-left text-sm">&copy; {{ __('Copyright') . ' ' . now()->year . ' ' . config('app.name', 'Poll Garden') }}</small>
         </div>
     </footer>
 </body>
--- resources/views/polls/show.blade.php
+++ resources/views/polls/show.blade.php
@@ -1,24 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
-<main class="flex-grow flex-shrink-0 bg-gray-50">
-    @include('components.page-title', ['title' => $poll->title])
+<form class="max-w-2xl" action="{{ route('polls.store') }}" method="POST">
+    @csrf
 
-    <div class="container py-12">
-        <form class="max-w-2xl" action="{{ route('polls.store') }}" method="POST">
-            @csrf
-
-            <fieldset>
-                <legend class="sr-only">{{ $poll->title }}</legend>
-                @foreach ($poll->options as $option)
-                    <label class="relative block pl-12 pr-4 py-4 mb-4 rounded-full bg-white cursor-pointer" for="option{{ $loop->index }}">
-                        <input class="appearance-none fancy-radio-button" id="option{{ $loop->index }}" name="options" type="radio">
-                        <span>{{ $option->name }}</span>
-                        <span class="float-right">15%</span>
-                    </label>
-                @endforeach
-            </fieldset>
-        </form>
-    </div>
-</main>
+    <fieldset>
+        <legend class="sr-only">{{ $poll->title }}</legend>
+        @foreach ($poll->options as $option)
+            <label class="relative block pl-12 pr-4 py-4 mb-4 rounded-full bg-white cursor-pointer" for="option{{ $loop->index }}">
+                <input class="appearance-none fancy-radio-button" id="option{{ $loop->index }}" name="options" type="radio">
+                <span>{{ $option->name }}</span>
+                <span class="float-right">15%</span>
+            </label>
+        @endforeach
+    </fieldset>
+</form>
 @endsection
