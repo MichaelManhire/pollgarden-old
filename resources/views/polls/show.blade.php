19027d9ea8fc4994ace809a9259e95eb97cd1f03
Michael Manhire <michaelmanhire@gmail.com>
2020-07-04 23:20:14 -0700

    Update show results for ballot box

--- resources/views/polls/show.blade.php
+++ resources/views/polls/show.blade.php
@@ -42,10 +42,10 @@
         </div>
 
         @auth
-        <article class="block mt-5" id="ballot-box">
+        <article class="block mt-5">
             @if (! Auth::user()->hasVoted($poll))
                 <h2 class="sr-only">Vote</h2>
-                <form action="{{ route('votes.store') }}" method="POST" x-data>
+                <form action="{{ route('votes.store') }}" method="POST" x-data="{ isShowingResults: false }">
                     @csrf
 
                     <fieldset>
@@ -53,14 +53,16 @@
                         @foreach ($poll->options as $option)
                             <div class="flex items-center {{ ($loop->first) ? '' : 'mt-4' }}">
                                 <label class="relative flex-grow block py-4 pl-12 pr-4 bg-gray-300 rounded-full cursor-pointer" for="{{ $option->id }}">
-                                    <input class="appearance-none fancy-radio-button"
-                                            id="{{ $option->id }}"
-                                            name="option_id"
-                                            type="radio"
-                                            value="{{ $option->id }}"
-                                            required
-                                            @change="$el.submit()">
-                                    <span>{{ $option->name }}</span>
+                                    <input class="fancy-radio-button"
+                                           id="{{ $option->id }}"
+                                           name="option_id"
+                                           type="radio"
+                                           value="{{ $option->id }}"
+                                           required
+                                           @change="$el.submit()">
+                                    <span class="relative z-10">{{ $option->name }}</span>
+                                    <span class="relative z-10 float-right font-bold" x-show="isShowingResults">{{ $option->percentage(count($poll->votes)) }}</span>
+                                    <span class="result-bar js-result-bar" data-percentage="{{ $option->percentage($poll->votes->count()) }}"></span>
                                 </label>
                                 <div class="flex-shrink-0 ml-3 text-green-600 invisible">
                                     @include('icons.checkmark', ['height' => '24', 'width' => '24'])
@@ -70,10 +72,14 @@
                     </fieldset>
 
                     <button class="sr-only" type="submit">Cast Your Vote</button>
+
+                    <div class="mt-2 ml-5">
+                        <button class="text-sm text-green-600 hover:underline" type="button" @click="isShowingResults = true; document.querySelectorAll('.js-result-bar').forEach(function (resultBar) { resultBar.style.maxWidth = resultBar.dataset.percentage })">Show Results</button>
+                    </div>
                 </form>
             @else
                 <h2 class="sr-only">Change Your Vote</h2>
-                <form action="{{ route('votes.update', $poll->usersVote(Auth::id())) }}" method="POST" x-data>
+                <form action="{{ route('votes.update', $poll->usersVote(Auth::id())) }}" method="POST" x-data="{ isShowingResults: false }">
                     @csrf
                     @method('PATCH')
 
@@ -82,18 +88,17 @@
                         @foreach ($poll->options as $option)
                             <div class="flex items-center {{ ($loop->first) ? '' : 'mt-4' }}">
                                 <label class="relative flex-grow block py-4 pl-12 pr-4 bg-gray-300 rounded-full cursor-pointer" for="{{ $option->id }}">
-                                    <input class="appearance-none fancy-radio-button"
+                                    <input class="fancy-radio-button"
                                            id="{{ $option->id }}"
                                            name="option_id"
                                            type="radio"
                                            value="{{ $option->id }}"
-                                           style="z-index: 1;"
                                            required
                                            {{ $poll->usersVote(Auth::id())->option_id === $option->id ? 'checked' : '' }}
                                            @change="$el.submit()">
-                                    <span class="relative" style="z-index: 1;">{{ $option->name }}</span>
-                                    <span class="relative float-right font-bold" style="z-index: 1;">{{ $option->percentage(count($poll->votes)) }}</span>
-                                    <span class="absolute top-0 left-0 z-0 w-full h-full bg-green-300 rounded-full" style="transition: max-width .3s ease; max-width: {{ $option->percentage($poll->votes->count()) }};"></span>
+                                    <span class="relative z-10">{{ $option->name }}</span>
+                                    <span class="relative z-10 float-right font-bold" x-show="isShowingResults">{{ $option->percentage(count($poll->votes)) }}</span>
+                                    <span class="result-bar js-result-bar" data-percentage="{{ $option->percentage($poll->votes->count()) }}"></span>
                                 </label>
                                 <div class="flex-shrink-0 ml-3 text-green-600 @if ($poll->usersVote(Auth::id())->option_id !== $option->id) invisible @endif">
                                     @include('icons.checkmark', ['height' => '24', 'width' => '24'])
@@ -103,6 +108,10 @@
                     </fieldset>
 
                     <button class="sr-only" type="submit">Change Your Vote</button>
+
+                    <div class="mt-2 ml-5">
+                        <button class="text-sm text-green-600 hover:underline" type="button" @click="isShowingResults = true; document.querySelectorAll('.js-result-bar').forEach(function (resultBar) { resultBar.style.maxWidth = resultBar.dataset.percentage })">Show Results</button>
+                    </div>
                 </form>
 
                 <form class="mt-3 text-right text-sm" action="{{ route('votes.destroy', $poll->usersVote(Auth::id())) }}" method="POST">
@@ -112,10 +121,6 @@
                     <button class="text-green-600 hover:underline" type="submit">Withdraw Your Vote</button>
                 </form>
             @endif
-
-            <div class="mt-2 ml-5">
-                <button class="text-sm text-green-600 hover:underline" type="button" @click="isShowingResults = true">Show Results</button>
-            </div>
         </article>
         @endauth
     </x-panel>
