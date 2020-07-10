25975a01d97637258d2385b6395992b08cab4319
Michael Manhire <michaelmanhire@gmail.com>
2020-07-09 21:15:10 -0700

    Fix a bug where PurgeCSS was removing category badge styles

--- webpack.mix.js
+++ webpack.mix.js
@@ -22,6 +22,24 @@ mix.sass('resources/sass/app.scss', 'public/css')
     })
     .purgeCss({
         content: [path.join(__dirname, 'resources/views/**/*.php')],
+        whitelist: [
+            'bg-green-100',
+            'text-green-800',
+            'bg-blue-100',
+            'text-blue-800',
+            'bg-yellow-100',
+            'text-yellow-800',
+            'bg-teal-100',
+            'text-teal-800',
+            'bg-red-100',
+            'text-red-800',
+            'bg-pink-100',
+            'text-pink-800',
+            'bg-orange-100',
+            'text-orange-800',
+            'bg-gray-100',
+            'text-gray-800',
+        ],
     })
 
 if (mix.inProduction()) {
